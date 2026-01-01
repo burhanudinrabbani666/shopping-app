2ca1c4534c5f4a85649fb5f7bee757f361581be8
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-01-01 21:11:31 +0700

    feat: add middleware

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const express = require("express");
 
 const app = express();
 
+app.use("/", (req, res, next) => {
+  console.log("in the middleware");
+
+  next(); // Allows the request to continue to the next middleware
+});
+
+app.use("/", (req, res, next) => {
+  // sending response to client
+  res.send(`<h1>Hello from express.js</h1>`);
+});
+
 // Creating Server
 const server = http.createServer(app);
 server.listen(3000);
